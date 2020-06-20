84a005d81b76e0fee5b658afe9617f2e4b254305
e-nechkoska <e.nechkoska@gmail.com>
2020-06-20 16:49:33 +0200

    add index.js in shared

---
+++ Local-library/shared/index.js
@@ -0,0 +1,7 @@
+const ValidationError = require('./validation-error');
+const AlreadyExistsError = require('./already-exists-error');
+
+module.exports = {
+  ValidationError,
+  AlreadyExistsError
+};
