362c3993de3e5737809b9722252fc948822f90e8
e-nechkoska <e.nechkoska@gmail.com>
2020-06-20 16:51:36 +0200

    adjust error handling

--- Local-library/app.js
+++ Local-library/app.js
@@ -50,8 +50,11 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (err.status === 406) {
+    res.json({data: err.validationErrors});
+  } else {
+    res.status(err.status || 500).json({data: err.stack});
+  }
 });
 
 module.exports = app;
