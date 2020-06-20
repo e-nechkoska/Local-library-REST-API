335eeffb97221f5654d9f487e27d43a6639ec810
e-nechkoska <e.nechkoska@gmail.com>
2020-06-20 16:52:44 +0200

    make createGenreController RESTful

--- Local-library/controllers/genre/createGenreController.js
+++ Local-library/controllers/genre/createGenreController.js
@@ -1,45 +1,36 @@
 const Genre = require('../../models/genre');
+const { AlreadyExistsError, ValidationError } = require('../../shared');
 
 const { body, validationResult } = require('express-validator');
 
-const genreCreateGet = function(req, res) {
-  res.render('genre_form', { title: 'Create Genre', genre: {} });
-};
-
-const genreCreatePost = [
+const genreCreate = [
   body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
 
   (req, res, next) => {
     const errors = validationResult(req);
-    let genre = new Genre({ 
-      name: req.body.name 
+
+    const genre = new Genre({
+      name: req.body.name
     });
 
     if(!errors.isEmpty()) {
-      res.render('genre_form', { 
-        title: 'Create Genre', 
-        genre: genre, 
-        errors: errors.array() 
-      });
+      next(new ValidationError(errors));
     } else {
-      Genre.findOne({'name': req.body.name})
+      Genre.findOne({name: req.body.name})
       .exec()
-      .then(foundGenre => {
-        if(foundGenre) {
-          res.redirect(foundGenre.url);
-        }
-        else {
-          genre.save()
-          .then(() => {
-            res.redirect(genre.url);
-          }).catch(error => next(error));
-        }
+      .then(genreExists => {
+        if(genreExists) {
+          const error = new AlreadyExistsError('Genre already exists!');
+          next(error);
+        } else {
+          genre.save();
+          res.status(201).json({data: genre});
+        }        
       }).catch(error => next(error));
     }
   }
 ];
 
 module.exports = {
-  genreCreateGet,
-  genreCreatePost
+  genreCreate
 };
