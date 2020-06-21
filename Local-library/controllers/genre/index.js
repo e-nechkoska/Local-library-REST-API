d60d5607fa0eb9df3dfb38e88a15ddddce07891b
e-nechkoska <e.nechkoska@gmail.com>
2020-06-21 18:39:02 +0200

    export genreCreate module

--- Local-library/controllers/genre/index.js
+++ Local-library/controllers/genre/index.js
@@ -1,13 +1,12 @@
 const { genreList, genreDetail } = require('./getGenreController');
-const { genreCreateGet, genreCreatePost } = require('./createGenreController');
+const { genreCreate } = require('./createGenreController');
 const { genreUpdateGet, genreUpdatePost } = require('./updateGenreController');
 const { genreDeleteGet, genreDeletePost } = require('./deleteGenreController');
 
 module.exports = {
   genreList,
   genreDetail,
-  genreCreateGet,
-  genreCreatePost,
+  genreCreate,
   genreUpdateGet,
   genreUpdatePost,
   genreDeleteGet,
