95f6508de528bda011b747605d6ebbe5f85ad0fd
e-nechkoska <e.nechkoska@gmail.com>
2020-06-22 15:39:42 +0200

    import/export new Errors

--- Local-library/shared/index.js
+++ Local-library/shared/index.js
@@ -1,9 +1,11 @@
 const ValidationError = require('./validation-error');
 const AlreadyExistsError = require('./already-exists-error');
-const ForbiddenError = require('./forbidden-error');
+const DeleteGenreValidationError = require('./delete-genre-validation-error');
+const ResourceNotFoundError = require('./resource-not-found-error');
 
 module.exports = {
   ValidationError,
   AlreadyExistsError,
-  ForbiddenError
+  DeleteGenreValidationError,
+  ResourceNotFoundError
 };
