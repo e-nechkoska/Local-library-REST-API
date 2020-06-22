7c6741cdbb24f62aa037309501809328f549b81c
e-nechkoska <e.nechkoska@gmail.com>
2020-06-22 17:21:32 +0200

    make bookinstaceList & bookinstanceDetail RESTful

--- Local-library/controllers/bookinstance/getBookinstanceController.js
+++ Local-library/controllers/bookinstance/getBookinstanceController.js
@@ -1,36 +1,30 @@
 const BookInstance = require('../../models/bookinstance');
 const { compareStrings } = require('../../utils');
+const { ResourceNotFoundError } = require('../../shared');
 
-const bookinstanceList = function(req, res, next) {
+const bookinstanceList = (req, res, next) => {
   BookInstance.find()
-  .populate('book')
-  .exec()
-  .then(bookinstanceList => {
-    bookinstanceList.sort((first, second) => {
-      return compareStrings(first.book.title, second.book.title);
-    });
-    res.render('bookinstance_list', {
-      title: 'Book Instance List', 
-      bookinstanceList: bookinstanceList
-    });
-  }).catch(error => next(error));
+    .populate('book')
+    .exec()
+    .then(bookinstances => {
+      bookinstances.sort((first, second) => {
+        return compareStrings(first.book.title, second.book.title);
+      });
+      res.status(200).json({data: bookinstances});
+    }).catch(error => next(error));
 };
 
-const bookinstanceDetail = function(req, res, next) {
+const bookinstanceDetail = (req, res, next) => {
   BookInstance.findById(req.params.id)
-  .populate('book')
-  .exec()
-  .then(bookinstance => {
-    if(bookinstance === null) {
-      let error = new Error('Book copy not found');
-      error.status = 404;
-      return next(error);
-    }
-    res.render('bookinstance_detail', {
-      title: 'Copy' + bookinstance.book.title, 
-      bookinstance: bookinstance
-    });
-  }).catch(error => next(error));
+    .populate('book')
+    .exec()
+    .then(bookinstance => {
+      if(bookinstance === null) {
+        const error = ResourceNotFoundError('Bookinstance not found');
+        return next(error);
+      }
+      res.status(200).json({data: bookinstance});
+    }).catch(error => next(error));
 };
 
 module.exports = {
