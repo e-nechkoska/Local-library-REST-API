bfaeba6bbdc6c54369c691be4b82737ef324662f
e-nechkoska <e.nechkoska@gmail.com>
2020-06-22 15:36:53 +0200

    make genreUpdate RESTful

--- Local-library/controllers/genre/updateGenreController.js
+++ Local-library/controllers/genre/updateGenreController.js
@@ -2,47 +2,36 @@ const Genre = require('../../models/genre');
 
 const { body, validationResult } = require('express-validator');
 
-const renderGenreForm = (res, errors = null) => {
-  res.render('genre_form', {
-    title: 'Update Genre',
-    genre: genre,
-    errors: errors 
-  })
-};
+const { ValidationError, ResourceNotFoundError } = require('../../shared');
 
-const genreUpdateGet = function(req, res, next) {
-  Genre.findById(req.params.id).exec().then(genre => {
-    if(genre === null) {
-      let error = new Error('Genre not found');
-      error.status = 404;
-      return next(error);
-    }
-    renderGenreForm(res);
-  }).catch(err => next(err));
-};
-
-const genreUpdatePost = [
+const genreUpdate = [
   body('name', 'Genre name must not be empty').trim().isLength({min: 1}).escape(),
 
   (req, res, next) => {
     const errors = validationResult(req);
-    let genre = new Genre({ 
+    let genre = new Genre({
       name: req.body.name,
       _id: req.params.id
     });
 
     if(!errors.isEmpty()) {
-      renderGenreForm(res, errors.array());
+      next(new ValidationError(errors));
     } else {
-      Genre.findByIdAndUpdate(req.params.id, genre).exec()
-      .then(updatedGenre => {
-        res.redirect(updatedGenre.url);
-      }).catch(error => next(error));
+      Genre.findOneAndUpdate({_id: req.params.id}, genre)
+        .exec()
+        .then(foundGenre => {
+          if(foundGenre === null) {
+            const error = new ResourceNotFoundError('Genre not found');
+            return Promise.reject(error);
+          }
+          return Genre.findById(req.params.id).exec();
+        })
+        .then(updatedGenre => res.status(200).json({data: updatedGenre}))
+        .catch(error => next(error));
     }
   }
 ];
 
 module.exports = {
-  genreUpdateGet,
-  genreUpdatePost
+  genreUpdate
 };
