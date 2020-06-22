9aaacf627d8ee50010e68e7444e5453fe2b1cd6f
e-nechkoska <e.nechkoska@gmail.com>
2020-06-22 17:20:47 +0200

    make bookinstanceCreate RESTful

--- Local-library/controllers/bookinstance/createBookinstanceController.js
+++ Local-library/controllers/bookinstance/createBookinstanceController.js
@@ -1,63 +1,32 @@
 const BookInstance = require('../../models/bookinstance');
-const Book = require('../../models/book');
-
-const { validationResult } = require('express-validator');
-const statuses = require('../../models/statuses');
-
 const validateBookinstance = require('./bookinstanceValidation');
-const compareStrings = require('../../utils/compare-strings');
+const { ValidationError } = require('../../shared');
+const { validationResult } = require('express-validator');
 
-const renderBookinstanceForm = (res, books, bookinstance, errors = null) => {
-  res.render('bookinstance_form', {
-    title: 'Create BookInstance', 
-    bookList: books,
-    statuses: statuses,
-    bookinstance: bookinstance,
-    errors: errors
-  });
-};
+const bookinstanceCreate = [
+  validateBookinstance,
 
-const bookinstanceCreateGet = function(req, res, next) {
-  Book.find({}, 'title')
-  .exec()
-  .then(books => {
-    books.sort(function(a, b) {
-      return compareStrings(a.title, b.title);
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const bookinstance = new BookInstance({
+      book: req.body.bookId,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      dueBack: req.body.dueBack
     });
 
-    const bookinstance = { book: books[0]._id };
-    renderBookinstanceForm(res, books, bookinstance);
-  }).catch(error => next(error));
-};
-
-const bookinstanceCreateMiddleware = (req, res, next) => {
-  const errors = validationResult(req);
-  let bookinstance = new BookInstance({
-    book: req.body.book,
-    imprint: req.body.imprint,
-    status: req.body.status,
-    dueBack: req.body.dueBack
-  });
-  if(!errors.isEmpty()) {
-    Book.find({}, 'title')
-    .exec()
-    .then(books => {
-      renderBookinstanceForm(res, books, bookinstance, errors.array());
-    }).catch(error => next(error));
-  }  else {
+    if(!errors.isEmpty()) {
+      const error = new ValidationError(errors);
+      return next(error);
+    }
     bookinstance.save()
-    .then(bookinstance => {
-      res.redirect(bookinstance.url);
-    }).catch(error => next(error));
+      .then(result => {
+        res.status(201).json({data: result});
+      }).catch(error => next(error));
   }
-};
-
-const bookinstanceCreatePost = [
-  validateBookinstance,
-  bookinstanceCreateMiddleware
- ];
+];
 
 module.exports = {
-  bookinstanceCreateGet,
-  bookinstanceCreatePost
+  bookinstanceCreate
 };
