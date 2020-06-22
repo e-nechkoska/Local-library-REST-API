45a1d1e981cf1398846d7b5e1b5c1e0e38d497ab
Hristijan Dimitrieski <Hristijan.Dimitrieski@netcetera.com>
2020-06-22 23:48:17 +0200

    make authorList & authorDetail RESTful

--- Local-library/controllers/author/getAuthorController.js
+++ Local-library/controllers/author/getAuthorController.js
@@ -1,58 +1,29 @@
-/**
- * TODOs
- * 
- * 1. Use promises instead of callbacks
- * 2. Create separate controllers for get, create, update, delete actions
- * 3. Rename variables to cammelCase
- * 4. Create "services": Move the null check there and reject the promise if needed.
- * 5. Improve error handling. Create Error classes. Ex. NotFound
- * 
- * Read: https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_and_retrieving_form_data
-*/
+const Author = require('../../models/author');
+const { ResourceNotFoundError } = require('../../shared');
 
-let Author = require('../../models/author');
-let Book = require('../../models/book');
-
-let authorList = function (req, res, next) {
+const authorList = (req, res, next) => {
   Author.find()
-  .populate('author')
-  .sort([['familyName', 'ascending']])
-  .exec()
-  .then(authors => {
-    res.render('author_list', {
-      title: 'Author List', 
-      authorList: authors
-    });
-  }).catch(error => next(error));
+    .populate('author')
+    .sort({'familyName': 1})
+    .exec()
+    .then(authors => {
+      res.status(200).json({data: authors});
+    }).catch(error => next(error));
 };
 
-let authorDetail = function (req, res, next) {
-  const authorPromise = Author.findById(req.params.id).exec();
-  const authorBooksPromise = Book.find({'author': req.params.id}, 'title summary').exec();
-  Promise.all([authorPromise, authorBooksPromise])
-  .then((results) => {
-    /**
-     * Results is a list of two objects: author and book.
-     * 
-     * Same as:
-     * const author = results[0];
-     * const book = results[1];
-    */
-    const [author, authorBooks] = results;
-    
-    if(author === null) {
-      let err = new Error('Author not found');
-      err.status = 404;
-      return next(err);
-    }
-    res.render('author_detail', {
-      title: 'Author Detail', 
-      author: author, 
-      authorBooksList: authorBooks});
-  }).catch(error => next(error)); 
+const authorDetail = (req, res, next) => {
+  Author.findById(req.params.id)
+    .exec()
+    .then(author => {
+      if(author === null) {
+        const error = new ResourceNotFoundError('Author not found');
+        return next(error);
+      }
+      res.status(200).json({data: author});
+    }).catch(error => next(error));
 };
 
 module.exports = {
   authorList,
   authorDetail
-}
+};
