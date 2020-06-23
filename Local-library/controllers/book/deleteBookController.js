1d8561e83466e54b2baa0b82be72713e9ed05353
e-nechkoska <e.nechkoska@gmail.com>
2020-06-23 12:35:19 +0200

    make bookDelete RESTful

--- Local-library/controllers/book/deleteBookController.js
+++ Local-library/controllers/book/deleteBookController.js
@@ -1,48 +1,27 @@
 const Book = require('../../models/book');
 const BookInstance = require('../../models/bookinstance');
+const { DeleteBookValidationError, ResourceNotFoundError } = require('../../shared');
 
-const renderBookDelete = (res, book, bookInstances) => {
-  res.render('book_delete', {
-    title: 'Delete Book',
-    book: book,
-    copies: bookInstances
-  });
-};
-
-const bookDeleteGet = function(req, res) {
-  const bookFindByIdPromise = Book.findById(req.params.id).exec();
-  const bookInstanceFindPromise = BookInstance.find({'book': req.params.id}).exec();
-
-  Promise.all([bookFindByIdPromise, bookInstanceFindPromise])
-  .then((results) => {
-    const [book, bookInstances] = results;
-    if(book === null) {
-      res.redirect('/catalog/books');
-    } else {
-      renderBookDelete(res, book, bookInstances);
-    }
-  }).catch(error => next(error));
-};
-
-const bookDeletePost = function(req, res, next) {  
-  const bookFindByIdPromise = Book.findById(req.body.bookid).exec();
-  const bookInstanceFindPromise = BookInstance.find({'book': req.body.bookid}).exec();
-
-  Promise.all([bookFindByIdPromise, bookInstanceFindPromise])
-  .then((results) => {
-    const [book, bookInstances] = results;
-    if(bookInstances.length > 0) {
-      renderBookDelete(res, book, bookInstances);
-    } else {
-      Book.findByIdAndRemove(req.body.bookid).exec()
-      .then(() => {
-        res.redirect('/catalog/books');
-      }).catch(error => next(error))
-    }
-  }).catch(error => next(error));
+const bookDelete = (req, res, next) => {
+  BookInstance.find({'book': req.params.id})
+    .exec()
+    .then(bookinstances => {
+      if(bookinstances.length > 0) {
+        const error = new DeleteBookValidationError('Delete the following bookinstances before deleting this book: ', bookinstances);
+        return Promise.reject(error);
+      }
+      return Book.findById(req.params.id).exec();
+    }).then(foundBook => {
+      if(foundBook === null) {
+        const error = new ResourceNotFoundError('Book not found');
+        return Promise.reject(error);
+      }
+      return Book.findByIdAndDelete(req.params.id).exec();
+    }).then(() => {
+      res.status(204).end();
+    }).catch(error => next(error));
 };
 
 module.exports = {
-  bookDeleteGet,
-  bookDeletePost
+  bookDelete
 };
