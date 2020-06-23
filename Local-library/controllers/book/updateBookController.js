6ed5d04fb8cb92623f9242811a2e3b22d606148e
e-nechkoska <e.nechkoska@gmail.com>
2020-06-23 21:35:42 +0200

    add partialBookUpdate controller

--- Local-library/controllers/book/updateBookController.js
+++ Local-library/controllers/book/updateBookController.js
@@ -1,5 +1,5 @@
 const Book = require('../../models/book');
-const bookValidation = require('./bookValidation');
+const { bookValidation, partialBookValidation } = require('./bookValidation');
 const { ValidationError, ResourceNotFoundError } = require('../../shared');
 const { validationResult } = require('express-validator');
 
@@ -36,6 +36,38 @@ const bookUpdate = [
   }
 ];
 
+const partialBookUpdate = [
+  partialBookValidation,
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+      const error = new ValidationError(errors);
+      return next(error);
+    }
+
+    Book.findById(req.params.id).lean().exec().then(originalBook => {
+      if(originalBook === null) {
+        const error = new ResourceNotFoundError('Book not found');
+        return Promise.reject(error);
+      }
+
+      const book = new Book({
+        ...originalBook,
+        ...req.body
+      });
+
+      return Book.update({ _id: req.params.id }, book).exec();
+      // OR return Book.update({ _id: req.params.id }, req.body).exec();    
+    })
+    .then(() => Book.findById(req.params.id))
+    .then(updatedBook => res.status(200).json({data: updatedBook}))
+    .catch(error => next(error));
+  }
+];
+
 module.exports = {
-  bookUpdate
+  bookUpdate,
+  partialBookUpdate
 };
