afe782f8a31c922265d4f6350ba283c39fb3d54d
e-nechkoska <e.nechkoska@gmail.com>
2020-06-23 12:33:37 +0200

    make authorCreate RESTful

--- Local-library/controllers/author/createAuthorController.js
+++ Local-library/controllers/author/createAuthorController.js
@@ -1,48 +1,32 @@
 const Author = require('../../models/author');
 const validateAuthor = require('./authorValidation');
-
+const { ValidationError } = require('../../shared');
 const { validationResult } = require('express-validator');
 
-const renderAuhtorForm = (res, author = {}, errors = null) => {
-  res.render('author_form', {
-    title: 'Create Author',
-    author: author,
-    errors: errors
-  });
-};
-
-const authorCreateGet = function (req, res) {
-  renderAuhtorForm(res);
-};
-
-const createAuthor = (body) => {
-  return new Author({
-    firstName: body.firstName,
-    familyName: body.familyName,
-    dateOfBirth: body.dateOfBirth,
-    dateOfDeath: body.dateOfDeath
-  });
-};
-
-const createAuthorMiddleware = (req, res, next) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    renderAuhtorForm(res, req.body, errors.array());
-  } else {
-    let author = createAuthor(req.body);
-    author.save()
-      .then(() => res.redirect(author.url))
-      .catch(error => next(error));
-  }
-};
-
-const authorCreatePost = [
+const authorCreate = [
   validateAuthor,
-  createAuthorMiddleware
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const author = new Author({
+      firstName: req.body.firstName,
+      familyName: req.body.familyName,
+      dateOfBirth: req.body.dateOfBirth,
+      dateOfDeath: req.body.dateOfDeath
+    });
+
+    if(!errors.isEmpty()) {
+      const error = new ValidationError(errors);
+      return next(error);
+    }
+    author.save()
+      .then(createdAuthor => {
+        res.status(201).json({data: createdAuthor});
+      }).catch(error => next(error));
+  }
 ];
 
 module.exports = {
-  authorCreateGet,
-  authorCreatePost
+  authorCreate
 };
